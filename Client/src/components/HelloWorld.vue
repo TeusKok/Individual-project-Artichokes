74ff3a0d05988456f1749dea621e6335cbf1385e
Teus Kok <tkok@sogyo.nl>
2023-11-13 12:10:40 +0100

    The Client can now make calls to the API

--- Client/src/components/HelloWorld.vue
+++ Client/src/components/HelloWorld.vue
@@ -1,9 +1,18 @@
+
 <script setup lang="ts">
 import { ref } from 'vue'
+const response = (await fetch("http://localhost:5173/api/weatherforecast"))
+const report = await response.json();
+
 
 defineProps<{ msg: string }>()
 
+
+
+
 const count = ref(0)
+const wreport = ref(report)
+//const forecast = ref(weatherforecast)
 </script>
 
 <template>
@@ -11,9 +20,11 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
+    {{ wreport }}
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
+      
     </p>
   </div>
 
--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -4,4 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5042/',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace('/api', ''),
+      },
+    }
+  }
 })
+
+
