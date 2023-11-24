84e61b6a6e725db03856d78c564e932b85349d33
Teus Kok <tkok@sogyo.nl>
2023-11-24 13:38:47 +0100

    Implemented shuffling of GardenStock and discardPile.

--- Domain/Artichokes/DrawPile.cs
+++ Domain/Artichokes/DrawPile.cs
@@ -2,34 +2,31 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Artichokes;
 
-public class DrawPile : ICardCollection{
-    private List<ICard> drawPile = new List<ICard>();
+public class DrawPile {
+    private readonly List<ICard> Cards = new List<ICard>();
 
     public int NumberOfCards(){
-        return drawPile.Count;
+        return Cards.Count;
     }
 
     public DrawPile(){
         for(int i =0; i<10; i++){
-            drawPile.Add(new Artichoke());
+            Cards.Add(new Artichoke());
         }
     }
 
     
     public ICard GetTopCard(){
-        return drawPile[0];
+        return Cards[0];
     }
 
     public void RemoveTopCard(){
-        drawPile.RemoveAt(0);
+        Cards.RemoveAt(0);
     }
 
     public void AddToPile(List<ICard> cards){
-        drawPile.AddRange(cards);
+        Cards.AddRange(cards);
     }
 
-    public void shuffle()
-    {
-        throw new NotImplementedException();
-    }
+    
 } 
